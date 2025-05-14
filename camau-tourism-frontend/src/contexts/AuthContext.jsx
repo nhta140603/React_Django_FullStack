d8a431d2a538533fd547da74baf0494b53b44c41
Nguyễn Hữu Tuấn Anh <nhta14141@gmail.com>
2025-05-14 22:15:24 +0700

    cap nhat background

--- camau-tourism-frontend/src/contexts/AuthContext.jsx
+++ camau-tourism-frontend/src/contexts/AuthContext.jsx
@@ -8,16 +8,10 @@ export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true)
   useEffect(() => {
-    const token = Cookies.get('accessToken');
-    if (token) {
-      getInfoUser()
-        .then(profile => setUser(profile))
-        .catch(() => setUser(null))
-        .finally(() => setLoading(false));
-    } else {
-      setUser(null);
-      setLoading(false);
-    }
+    getInfoUser()
+      .then(profile => setUser(profile))
+      .catch(() => setUser(null))
+      .finally(() => setLoading(false));
   }, []);
 
   const login = async (loginData) => {
