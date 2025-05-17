9550f9ec84ead4f44fb2e60742f9862b193e1a02
Nguyễn Hữu Tuấn Anh <nhta14141@gmail.com>
2025-05-17 15:06:43 +0700

    cap nhat header mobile

--- camau-tourism-admin/src/api/api_generics.jsx
+++ camau-tourism-admin/src/api/api_generics.jsx
@@ -4,9 +4,7 @@ const API_URL =
     : (import.meta.env.VITE_API_URL || '/choreo-apis/djangoreactapp/camautourismbackend/v1/api/admin/');
 
 const fetchWithAuth = async (url, options = {}) => {
-  const token = localStorage.getItem("accessToken");
   let headers = {
-    "Authorization": `Bearer ${token}`,
     ...options.headers,
   };
   if (options.body instanceof FormData) {
@@ -16,7 +14,7 @@ const fetchWithAuth = async (url, options = {}) => {
   } else if (!headers['Content-Type']) {
     headers['Content-Type'] = 'application/json';
   }
-  const res = await fetch(url, { ...options, headers });
+  const res = await fetch(url, { ...options, headers, credentials: 'include' });
   if (!res.ok) throw new Error('Lỗi API');
   if (res.status === 204) return null;
   const contentType = res.headers.get("content-type");
